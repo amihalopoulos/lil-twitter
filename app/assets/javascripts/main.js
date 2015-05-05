fbc76a7a479f4fd0ebf032d47575754960c567f7
amihalopoulos & Harry Park <pair+amihalopoulos.imharrypark@devbootcamp.com>
2015-05-05 11:17:44 -0400

    implement trending hashtags on page load

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
       var output = template({tweets: response});
       $('#tweets-container').html(output);
   });
+
+  var source2   = $("#trending-hashtags-template").html();
+  var template2 = Handlebars.compile(source2); // template is a function.
+  $.get('/hashtags/popular').then(function(response){
+      var output = template2({hashtags: response});
+      $('#trends-container').html(output);
+  });
+
 });
